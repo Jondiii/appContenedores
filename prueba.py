f61641c3d5837d76a789145620e9e18c33760941
Jon Díaz <jondi@opendeusto.es>
2021-02-09 16:48:59 +0100

    prueba ventana

--- prueba.py
+++ prueba.py
@@ -1,1 +1,4 @@
-preuab
+import PySimpleGUI as sg
+
+#https://realpython.com/pysimplegui-python/
+sg.Window(title="Prueba", layout= [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]], margins=(500, 200)).read()
