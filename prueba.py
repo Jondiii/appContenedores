31990488c1376975525dc0cb04fa469c9df1c2c3
haizea <haizearodriguez@opendeusto.es>
2021-02-10 14:27:42 +0100

    prueba haizea

--- prueba.py
+++ prueba.py
@@ -1,4 +1,4 @@
 import PySimpleGUI as sg
 
 #https://realpython.com/pysimplegui-python/
-sg.Window(title="Prueba", layout= [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]], margins=(500, 200)).read()
+sg.Window(title="Planificador de rutas", layout= [[sg.Text("Planificador de rutas")], [sg.Button("OK")]], margins=(500, 200)).read()
